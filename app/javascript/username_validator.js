528234244710bd12208d8b3aeb7cfc395c406db6
Al Palaran <aljade1902@gmail.com>
2024-06-26 18:24:41 +0100

    add length validation

--- app/javascript/username_validator.js
+++ app/javascript/username_validator.js
@@ -1,11 +1,20 @@
 const usernameInput = document.getElementById('username');
 const usernameAlertContainer = document.getElementById('valid-username-alert');
 usernameInput.addEventListener('input', () => {
+  console.log('inpuit');
   if(usernameAlertContainer){
     usernameAlertContainer.innerHTML = '' // clear existing alerts  
   }
   const apiUrl = 'api/v1/validate_username'
-  fetch(`${apiUrl}?query=${encodeURIComponent(usernameInput.value)}`)
+  var usernameValue = encodeURIComponent(usernameInput.value)
+  if(usernameValue.length == 0){
+    const message = 'Username cannot be blank.'
+    usernameAlertContainer.innerHTML = `<div style="color: red;">${message}</div>`;
+  }else if(usernameValue.length < 5){
+    const message = 'Username cannot be shorter than 5 characters.'
+    usernameAlertContainer.innerHTML = `<div style="color: red;">${message}</div>`;
+  }else{
+    fetch(`${apiUrl}?query=${usernameValue}`)
     .then(response => response.json())
     .then(data => {
       const status = data.status;
@@ -14,4 +23,5 @@ usernameInput.addEventListener('input', () => {
       usernameAlertContainer.innerHTML = `<div style="color: ${color};">${message}</div>`;
     })
     .catch(error => console.error('Error fetching posts:', error));
+  }
 });
