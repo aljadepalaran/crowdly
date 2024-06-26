7cf100dc08d73de8045d2bd5dda7136b8131b36c
Al Palaran <aljade1902@gmail.com>
2024-06-26 16:34:34 +0100

    add admin area

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'login', to: 'login#new'
+
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+  end
+
   root "home#index"
 end
